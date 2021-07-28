3d1cb813d355b7ee7ab464495af8913650ffcec6
andrek <akhf91@gmail.com>
2021-07-29 00:26:50 +0200

    <build>(product)

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,23 +1,26 @@
-(async function() {
-    const ARTICLES = await getArticles()
-    for (article of ARTICLES) {
-        displayArticle(article)
+// fonction permettant de se lancer seul
+(async function() {                         // async pour attendre les reponse des promises
+    const ARTICLES = await getArticles()    // constantes + et attente des reponses
+    for (article of ARTICLES) {             // boucle pour crée chaque produits
+        displayArticle(article)             // affichage des produits
     }
 })()
 
+// fonction de requete et réponse
 function getArticles() {
-    return fetch("http://localhost:3000/api/furniture")
-        .then(function(httpBodyResponse) {
-            return httpBodyResponse.json()
+    return fetch("http://localhost:3000/api/furniture") // requete vers le lien et reponse
+        .then(function(httpBodyResponse) {              // promise 1
+            return httpBodyResponse.json()              // typage des données de la réponse
         })
-        .then(function(articles) {
+        .then(function(articles) {                      // promise 2 grace au retour du 1
             return articles              
         })
-        .catch(function(error) {
+        .catch(function(error) {                        // capture des erreur                
             alert(error)
         })
 }
 
+// fonction d'affichage des articles
 function displayArticle(article) {
     const templateItem = document.getElementById("templateArticle")
     const cloneItem = document.importNode(templateItem.content, true)
@@ -25,12 +28,20 @@ function displayArticle(article) {
     cloneItem.getElementById("imageUrl").textContent= article.imageUrl
     cloneItem.getElementById("name").textContent= article.name
     cloneItem.getElementById("price").textContent= article.price
+    cloneItem.getElementById("productLink").href += `?id=${article.id}`
 
-    document.getElementById("furniture").appendChild(cloneItem)
+    document.getElementById("furniture").appendChild(cloneItem)         // creation d'un enfant
 }
+
 
 //let price = 'article.price';
 //function number() {
 //   return Number.parseFloat(${price}).toFixed(2);
 // }
 //   console.log(`Prix : + number + €`);  // expected output: "xx.xx €"/
+
+
+
+
+
+
