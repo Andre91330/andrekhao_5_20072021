3ed552866a8902f27b043a5c51b5c4ca9c335ccd
andrek <akhf91@gmail.com>
2021-07-29 18:57:21 +0200

    <fix>

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -9,8 +9,8 @@
 // fonction de requete et réponse
 function getArticles() {
     return fetch("http://localhost:3000/api/furniture") // requete vers le lien et reponse
-        .then(function(httpBodyResponse) {              // promise 1
-            return httpBodyResponse.json()              // typage des données de la réponse
+        .then(function(httpResponse) {              // promise 1
+            return httpResponse.json()              // typage des données de la réponse
         })
         .then(function(articles) {                      // promise 2 grace au retour du 1
             return articles              
@@ -25,14 +25,15 @@ function displayArticle(article) {
     const templateItem = document.getElementById("templateArticle")
     const cloneItem = document.importNode(templateItem.content, true)
     
-    cloneItem.getElementById("imageUrl").textContent= article.imageUrl
+    cloneItem.getElementById("imageUrl").setAttribute("src", article.imageUrl)
     cloneItem.getElementById("name").textContent= article.name
     cloneItem.getElementById("price").textContent= article.price
-    cloneItem.getElementById("productLink").href += `?id=${article.id}`
+    cloneItem.getElementById("productLink").href += `?id=${article._id}`
 
     document.getElementById("furniture").appendChild(cloneItem)         // creation d'un enfant
 }
 
+console.log(document.getElementById('home'));
 
 //let price = 'article.price';
 //function number() {
--- frontend/js/product.js
+++ frontend/js/product.js
@@ -1,6 +1,18 @@
 monStokage = localStorage;
 
+fetch("http://localhost:3000/api/furniture")
+    .then(function(httpResponse) {
+        if (httpResponse.ok) {
+            return httpResponse.json();
+        }
+    })
+    .then(function(articles) {
+        console.log(articles);
+    })
 
+    .catch(function(error) {
+     alert(error)
+    });
 
 
 
