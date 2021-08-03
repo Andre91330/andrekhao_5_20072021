6aff102b9bbfd24aedf96ac94ff9a32c89a9f825
andrek <akhf91@gmail.com>
2021-08-03 23:19:52 +0200

    <test>

--- frontend/js/product.js
+++ frontend/js/product.js
@@ -28,36 +28,54 @@ function hydrateArticle(article) {
     document.getElementById("description-text").textContent= article.description
     document.getElementById("name").textContent= article.name
     document.getElementById("price").textContent= article.price    
-
 }
 
-// fonction 
-function getVarnishs() {                         
-    const varnish = getVarnishs()           // constantes 
-    for (varnish of varnishs) {             // boucle pour crée chaque produits
-        displayArticle(varnish)             // affichage des produits
+const espaceMessage = document.getElementById("message");      // element de message
+const bouton = document.getElementById("bouton");               // element bouton
+const varnishInput = document.getElementById("varnish");             // champs de choix
+
+let varnish;
+let varnishsTableau = [];
+
+    for (let varnishTableau of varnishsTableau) {
+    console.log(varnishTableau);
     }
-}
 
-// fonction de requete et réponse
-function getVarnishs() {
-    fetch(`http://localhost:3000/api/furniture/${varnishId}`) // requete vers le lien et reponse
-        .then(function(httpResponse) {              // promise 1
-            return httpResponse.json()              // type des données de la réponse
-        })
-        .then(function(varnishs) {                  // promise 2 grace au retour du 1
+    // fonction 
+    function getVarnishs() {                         
+        const varnish = getVarnishs()           // constantes 
+        for (varnish of varnishs) {             // boucle pour crée chaque produits
+        displayArticle(varnish)             // affichage des produits
+        }
+    }
+
+    // fonction de requete et réponse
+    function getVarnishs(varnish) {
+        fetch(`http://localhost:3000/api/furniture/${varnish}`) // requete vers le lien et reponse
+            .then(function(httpResponse) {              // promise 1
+                return httpResponse.json()              // type des données de la réponse
+            })
+            .then(function(varnishs) {                  // promise 2 grace au retour du 1
             return varnishs              
-        })
-        .catch(function(error) {                    // gestion d'erreur                
-            alert(error)
-        })
+            })
+            .catch(function(error) {                    // gestion d'erreur                
+                alert(error)
+            })
+    }
+
+    function displayArticle(){
+        const templateVarnish = document.getElementById("templateVarnish")
+        const cloneVarnish = document.importNode(templateVarnish.content, true)
+    
+        cloneVarnish.getElementById("varnish").textContent= article.varnish
+    
+        document.getElementById("color").appendChild(cloneVarnish)         // creation d'un enfant
+    }
+
+function valider(){
+    espaceMessage.immerHTML = "Merci du choix de finition";                // message de validation
 }
 
-function displayArticle(){
-    const templateVarnish = document.getElementById("templateVarnish")
-    const cloneVarnish = document.importNode(templateVarnish.content, true)
-    
-    cloneVarnish.getElementById("varnish").textContent= article.varnish
-    
-    document.getElementById("color").appendChild(cloneVarnish)         // creation d'un enfant
+function refuser(){
+    alert("Veuillez choisir votre verni");                                 // message de non validation
 }
