7fcbb236b0ec014351d571213372b1f39cc44b42
andrek <akhf91@gmail.com>
2021-09-09 16:19:19 +0200

    <fix>

--- frontend/js/basket.js
+++ frontend/js/basket.js
@@ -1,4 +1,4 @@
-const displayBasket = (basket) => {                                             // affichage des totaux du panier cumul
+const displayBasket = (basket) => {                                // affichage des totaux du panier cumul
     document.getElementById("productsNumber").innerText = basket.nbProducts;  
     document.getElementById("totalPrice").innerText = displayPrice(basket.priceTotal); 
 
@@ -6,7 +6,7 @@ const displayBasket = (basket) => {                                             
         return `${(price/100).toFixed(2)} €`;
     }
  
-    const productsEltName = document.querySelector(".finalItem");               // affichage des produits choisis et personnalisés
+    const productsEltName = document.querySelector(".finalItem");   // affichage des produits choisis et personnalisés
         for (let i = basket.products.length; i--;) {       
             const productName = basket.products[i].name        
             let trElt = document.createElement("tr"); 
@@ -15,7 +15,7 @@ const displayBasket = (basket) => {                                             
     console.log(productName);
         }
 
-    const productsEltPrice = document.querySelector(".unitPrice");               // affichage des tarif
+    const productsEltPrice = document.querySelector(".unitPrice");   // affichage des tarif
         for (let i = basket.products.length; i--;) {       
             const productPrice = basket.products[i].price        
             let trElt = document.createElement("tr"); 
@@ -25,21 +25,21 @@ const displayBasket = (basket) => {                                             
         }        
 };
 
-clearBasket.onclick = () => {                                                      // suppression du panier
+clearBasket.onclick = () => {                                      // suppression du panier
     localStorage.clear();
     document.location.reload();
 };
 
 // --- controle du formulaire avant envoi en LS
-const checkInputName = (input) => {
-    if (input.length <=1 || input.match(/[0-9]/g)){                                 // verifie <1 lettre et pas de chiffre
+const checkInputName = (input) => {                                // verifie <1 lettre et pas de chiffre
+    if (input.length <=1 || input.match(/[0-9]/g)){               
         alert("Nom ou Prénom mal saisi."); 
         return false;       
     }
     return true;   
 };
 
-const checkInputEmail = (input) => {                                                // verifie le format mail
+const checkInputEmail = (input) => {                               // verifie le format mail
     if (input.length <=1 || !input.match(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {           
         alert("Veuillez vérifier votre adresse E-mail.");
         return false;
@@ -47,7 +47,7 @@ const checkInputEmail = (input) => {                                            
     return true;
 };
 
-const checkInputAddress = (input) => {                                              // verifie <1 lettre
+const checkInputAddress = (input) => {                             // verifie <1 lettre
     if (input.length <=2) {
         alert("Veuillez vérifier votre adresse.");
         return false;
@@ -55,7 +55,7 @@ const checkInputAddress = (input) => {                                          
     return true;
 };
 
-const checkInputCity = (input) => {                                                  // verifie CP >5 chiffres
+const checkInputCity = (input) => {                                // verifie CP >5 chiffres
     if (input.length <=1 || !input.match(/^([0-9]{5}) (.*)$/g)){               
         alert("Veuillez corriger votre Code postal ou ville.");
         return false;
@@ -63,11 +63,11 @@ const checkInputCity = (input) => {                                             
     return true;
 };
 
- //--- creation du data client ---
+ //--- creation du data client
 const getValueFromInput = (input) =>    
     document.querySelector(`#${input}`).value;
 
-const buildContactData = () => {                                                    // creation des variables contact
+const buildContactData = () => {                                   // creation des variables contact selon le format du fetch post
     const lastName = getValueFromInput("lastName");
     const firstName = getValueFromInput("firstName");
     const email = getValueFromInput("email");
@@ -76,7 +76,7 @@ const buildContactData = () => {                                                
   
 
     if (
-        !checkInputName(lastName) ||                                                 // controle que chaque variable avant envoi du formulaire 
+        !checkInputName(lastName) ||                               // controle que chaque variable avant envoi du formulaire 
         !checkInputName(firstName) ||
         !checkInputAddress(address) ||
         !checkInputCity(city) ||
@@ -95,9 +95,8 @@ const buildContactData = () => {                                                
     };
 };
 
-//--- const buildProductsData = (basket) => basket.products.map(product => product_id);
-
-const buildProductsData = (basket) => {
+ //--- creation du data objet
+const buildProductsData = (basket) => {                             // creation des artilces
     const ids = [];
     for (let i = basket.products.length; i--;) {
             ids.push(basket.products[i]._id);
@@ -105,8 +104,7 @@ const buildProductsData = (basket) => {
         return ids;
 }; 
 
- 
-const buildObjectsForOrder = (basket) => {
+const buildObjectsForOrder = (basket) => {                          // format du fetch post
     const contact = buildContactData();
     const products = buildProductsData(basket);  
     return {
@@ -115,17 +113,22 @@ const buildObjectsForOrder = (basket) => {
     }   
 };
 
+const displayEmptyBasket = () => {                                  // en cas de panier vide
+    document.querySelector(".pageBasket").innerHTML = '<br/><div>Votre panier contient aucun article.</div><br/>';
+}
 
-
-// a modifier
 (() => {
     const basket = JSON.parse(localStorage.getItem("basket"));
-    displayBasket(basket);
+    if (basket && basket.products.length){
+        displayBasket(basket);
+    }else{
+        displayEmptyBasket();
+    } 
 
     const sendOrder = () => {
         const data = buildObjectsForOrder(basket);
     console.log(data);
-        if(!!data.contact){                                         // si contact =  true,  on execute le fectch, sinon on gere les erreurs
+        if(!!data.contact){                                         // gestion des erreurs ; si contact =  true,  on execute le fectch.
             fetch("http://localhost:3000/api/furniture/order", {
                 method: "post",
                 headers: {
